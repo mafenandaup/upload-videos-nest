6f00a3372f655b4b4c7f03aef40704689c26a8b0
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-18 14:56:32 -0300

    K

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   getHello(): string {
-    return 'Bem Vindo ao Projeto Upload de Vídeo';
+    return this.appService.getHello();
   }
 }
